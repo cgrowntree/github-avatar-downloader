1574bec236ed6f92bdf0bc339cbc340d87f01899
Christopher Rowntree <cgrowntree@gmail.com>
2018-01-23 03:26:18 +0000

    Added github auth token and ignored secret.js file in gitignore

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,25 @@
 var request = require('request');
+var github_token = require('./secrets');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+  request(url, function(err, res, body) {
+    cb(err, body);
+  });
+
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': 'github_token'
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
