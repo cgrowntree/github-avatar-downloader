4fad57560a94311d89c43c47a9bed6b69d22a370
Christopher Rowntree <cgrowntree@gmail.com>
2018-01-23 04:28:04 +0000

    Parsed data and looped through data to log all github profile images

--- download_avatars.js
+++ download_avatars.js
@@ -4,25 +4,25 @@ var github_token = require('./secrets');
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
-  request(url, function(err, res, body) {
-    cb(err, body);
-  });
-
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request',
-      'Authorization': 'github_token'
+      'Authorization': github_token
     }
   };
 
   request(options, function(err, res, body) {
-    cb(err, body);
+    var parseBody = JSON.parse(body);
+    cb(err, parseBody);
   });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
-  console.log("Result:", result);
+  for (var i = 0; i < result.length; i++) {
+    var contributor = result[i];
+    console.log(contributor.avatar_url);
+  }
+
 });
